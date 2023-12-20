fc247153f1ab6ee8f8549943f6fe8db2bd55365a
MaxPrsn <maxence.persine@gmai.com>
2023-12-20 10:37:29 +0100

    maj

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Clock from './components/Clock'
 import Slider from './components/Slider'
 import DemoEvent from './components/DemoEvent'
 import DemoForm from './components/DemoForm'
+import FormulaireAjoutPrenom from './components/FormulaireAjoutPrenom'
 
 function App() {
 
@@ -14,30 +15,7 @@ function App() {
 
   return (
     <>
-    <DemoForm />
-    <DemoEvent />
-    <Slider />
-    <Clock />
-      <h1>Hello {prenom} !</h1>
-      <p className='red'>Lorem ipsum dolor sit,m numquam ducimus id consequuntur. Harum in molestias non magni, sunt praesentium minus adipisci debitis, quidem nam natus laudantium beatae. Deserunt!</p>
-      <ul>
-        {liste.map((element, i) => <li key={i}>{element}</li>)}
-      </ul>
-      <p>
-        {
-          liste.map((element, i ) => {
-            return (
-              <>
-              <strong>{i} </strong><span>{element} </span>
-              </>
-            )
-          })
-        }
-      </p>
-      <Hello firstname="Dimitri"/>
-      <Hello firstname="Maxime"/>
-      <Hello firstname="Lucas"/>
-      <Hello><strong>Coucou</strong></Hello>
+    <FormulaireAjoutPrenom listePrenomsExistant={['Jean', 'Marie', 'Paul']} />
     </>
   )
 }
---
+++ src/components/FormulaireAjoutPrenom.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const FormulaireAjoutPrenom = ({ listePrenomsExistant }) => {
+  const [nouveauPrenom, setNouveauPrenom] = useState('');
+  const [listePrenoms, setListePrenoms] = useState(listePrenomsExistant);
+  const [erreur, setErreur] = useState('');
+
+  const handleChange = (e) => {
+    setNouveauPrenom(e.target.value.toUpperCase());
+    setErreur('');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (nouveauPrenom.trim() === '') {
+      setErreur('Le champ ne peut pas être vide.');
+      return;
+    }
+
+    if (listePrenoms.includes(nouveauPrenom)) {
+      setErreur('Cet utilisateur existe déjà.');
+      return;
+    }
+
+    setListePrenoms([...listePrenoms, nouveauPrenom]);
+    setNouveauPrenom('');
+    setErreur('');
+  };
+
+  return (
+    <div>
+      <h2>Liste des prénoms</h2>
+      <ul>
+        {listePrenoms.map((prenom, index) => (
+          <li key={index}>{prenom}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nouveau prénom : 
+          <input type="text" value={nouveauPrenom} onChange={handleChange} />
+        </label>
+        <button type="submit">Ajouter</button>
+      </form>
+      {erreur && <p style={{ color: 'red' }}>{erreur}</p>}
+    </div>
+  );
+};
+
+export default FormulaireAjoutPrenom;
